e8642791e15e813f1d17b24dbe888fab2e1ec233
Gustavo <gus20solji@gmail.com>
2019-03-06 19:03:50 -0600

    Send properties to a component

--- src/App.js
+++ src/App.js
@@ -2,18 +2,29 @@ import React, { Component } from 'react';
 //import logo from './logo.svg';
 import Container from 'react-bootstrap/Container'
 
+
 import './App.css';
 
 class App extends Component {
   render() {
+    const {name, message} = this.props;
     return (
      <Container>
-       <h1>Hola react!!</h1>
+      
+       <h1>Hola {name}</h1>
+       <p>{message}</p>
+           
      </Container>
       
    
     );
   }
 }
+
+App.defaultProps = {
+ name: "Gustavo",
+ message: "Aprendiendo react en 1,2,3"
+ 
+};
 
 export default App;
